234e735ca1bed24d836c72b085b2aeea704d07d4
Burence1 <burensomondi@gmail.com>
2021-05-10 17:56:30 +0300

    add admin dashboard

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Article,Editor,tags
+
+class EditorTestClass(TestCase):
+  #setup
+  def setUp(self):
+    self.Burens=Editor(first_name= 'Burens',last_name= 'Omondi', email= 'burensdev@gmail.com')
 
 # Create your tests here.
+
+#testing instance
+
+  def test_instance(self):
+    self.assertTrue(isinstance,(self.Burens,Editor))
+
+  def test_save(self):
+    self.Burens.save_editor()
+    editors=Editor.objects.all()
+    self.assertTrue(len(editors)>0)
