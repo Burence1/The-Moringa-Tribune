d6b961062f503a2d6e9989f05b0c469b135ac999
Burence1 <burensomondi@gmail.com>
2021-05-11 14:30:09 +0300

    add image

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
   path('',views.news_today,name='newsToday'),
   re_path('archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news,name = 'pastNews'),
-  path('search/',views.search_results,name='search_results')
+  path('search/',views.search_results,name='search_results'),
+  path('article/(\d+)', views.article, name='article')
 ]
+
+if settings.DEBUG:
+  urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
