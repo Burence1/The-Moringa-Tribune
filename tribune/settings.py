fc3d6742f2b3d98731e538c0486c8f788af1ca2a
Burence1 <burensomondi@gmail.com>
2021-05-12 13:43:03 +0300

    deployment

--- tribune/settings.py
+++ tribune/settings.py
@@ -29,8 +29,7 @@ MODE=config("MODE",default="dev")
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = config('DEBUG', default=False, cast=bool)
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
