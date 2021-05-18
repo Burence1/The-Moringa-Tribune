40d51306f9f284ec0e88d70047bd3570460506cd
Burence1 <burensomondi@gmail.com>
2021-05-18 11:32:51 +0300

    add email and subscriber logic

--- news/views.py
+++ news/views.py
@@ -2,8 +2,9 @@ from django import forms
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 import datetime as dt
-from .models import Article
+from .models import Article,NewsLetterRecipients
 from .forms import  NewsLetterForm
+from .email import send_welcome_email
 
 # Create your views here.
 def news_today(request):
@@ -12,9 +13,14 @@ def news_today(request):
   if request.method == 'POST':
     form = NewsLetterForm(request.POST)
     if form.is_valid():
-      print("valid")
-    else:
-      form=NewsLetterForm()
+      name = form.cleaned_data['your_name']
+      email = form.cleaned_data['email']
+      recipient = NewsLetterRecipients(name=name, email=email)
+      recipient.save()
+      send_welcome_email(name,email)
+      HttpResponseRedirect('news_today')
+  else:
+    form=NewsLetterForm()
   return render(request, 'all-news/today-news.html', {"date": date, "news":news,"letterForm":form})
 
 def convert_dates(dates):
